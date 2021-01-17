dc903bc60f4a3dca508f87af4ee6b885445ac0ca
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-17 15:21:22 +0900

    move_to_index 変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   
 
   def index
